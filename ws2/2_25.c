e3d190eee058a26ce87a5e1212109ffbb9218ece
Megan Ju <megan.ju@sjsu.edu>
2023-02-09 04:14:41 +0000

    2.25 Lab

---
+++ ws2/2_25.c
@@ -0,0 +1,30 @@
+/**
+ * Description: Exercise 2.25 from Zybooks
+ * Author names: Neel Patel, Megan Ju
+ * Author emails: neel.patel@sjsu.edu; megan.ju@sjsu.edu
+ * Last modified date: February 8, 2023
+ * Creation date: February 8, 2023
+ **/
+ 
+ #include <stdio.h>
+ 
+ /**
+ * This function divides two numbers given by user and prints the result
+ * Example: If input is 2000 2 then it will divide 2000 by 2 three times
+ *          and print the results
+ * Assumption: none
+ * Input parameters: none
+ * Returns: an integer
+**/
+ 
+ int main(void){
+ 	int user_num;
+ 	int x;
+ 	
+ 	scanf("%d", &user_num);
+ 	scanf("%d", &x);
+ 	printf("%d %d %d \n", user_num/x, user_num/x/x, user_num/x/x/x);
+
+ 	return 0;
+  
+ }
