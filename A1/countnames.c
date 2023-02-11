e94e0cd34c793745e65478252e0763642e8a07c6
Megan Ju <megan.ju@sjsu.edu>
2023-02-11 23:51:20 +0000

    starting assignment 1

---
+++ A1/countnames.c
@@ -0,0 +1,19 @@
+/**
+ * Description: This module implements mathematical operations on vectors.
+ * Author names: Megan Ju, Neel Patel
+ * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
+ * Last modified date: February 10, 2023
+ * Creation date: February 11, 2023
+ **/
+ 
+ #include <stdio.h>
+ 
+ /**
+ * Main function for counting naem occurrences in a file
+ * Assumption: none
+ * Input parameters: arc, argv
+ * Returns: 0 if successful, 1 if there was an error
+ **/
+ int main(void){
+ 
+ }
